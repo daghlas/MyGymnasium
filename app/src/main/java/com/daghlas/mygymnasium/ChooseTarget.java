8061e53ea561a1327a17710a9f30ac5072af6f7f
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-18 11:37:22 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/mygymnasium/ChooseTarget.java
@@ -0,0 +1,76 @@
+package com.daghlas.mygymnasium;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class ChooseTarget extends AppCompatActivity {
+
+    CardView bellyFat, keepFit, buildMuscle;
+    TextView belly, fit, muscle;
+    ProgressBar belly_progressBar, fit_progressBar, muscle_progressBar;
+    Button cancel;
+    private boolean isClicked = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_target);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+
+        bellyFat = findViewById(R.id.belly_fat);
+        keepFit = findViewById(R.id.keep_fit);
+        buildMuscle = findViewById(R.id.build_muscle);
+
+        belly = findViewById(R.id.belly_fat_text);
+        fit = findViewById(R.id.keep_fit_text);
+        muscle = findViewById(R.id.build_muscle_text);
+
+        belly_progressBar = findViewById(R.id.belly_fat_progress);
+        fit_progressBar = findViewById(R.id.keep_fit_progress);
+        muscle_progressBar = findViewById(R.id.build_muscle_progress);
+
+        cancel = findViewById(R.id.cancel);
+
+        //VISIBILITY CHECKS
+        belly_progressBar.setVisibility(View.INVISIBLE);
+        fit_progressBar.setVisibility(View.INVISIBLE);
+        muscle_progressBar.setVisibility(View.INVISIBLE);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        bellyFat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isClicked){
+                    bellyFat.setCardBackgroundColor(getResources().getColor(R.color.grey));
+                }else {
+                    bellyFat.setCardBackgroundColor(getResources().getColor(R.color.blue));
+                    belly_progressBar.setVisibility(View.VISIBLE);
+                    //next activity
+                    new Handler().postDelayed(()->{
+                        Intent intent = new Intent(ChooseTarget.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    },2000);
+                }
+                isClicked = !isClicked;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/daghlas/mygymnasium/SplashScreen.java
@@ -0,0 +1,51 @@
+package com.daghlas.mygymnasium;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+    public static int SPLASH_SCREEN = 3000;
+
+    CardView card1,card2, card3;
+    TextView ash, dev;
+    Animation enterLeft, rotateIn, enterBottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        card1 = findViewById(R.id.card1);
+        card2 = findViewById(R.id.card2);
+        card3 = findViewById(R.id.card3);
+
+        ash = findViewById(R.id.ash);
+        dev = findViewById(R.id.developer);
+
+        enterLeft = AnimationUtils.loadAnimation(this, R.anim.enter_left);
+        rotateIn = AnimationUtils.loadAnimation(this, R.anim.rotate_in);
+        enterBottom = AnimationUtils.loadAnimation(this, R.anim.enter_bottom);
+
+        ash.startAnimation(enterLeft);
+        dev.startAnimation(enterBottom);
+        card1.startAnimation(rotateIn);
+        card2.startAnimation(rotateIn);
+        card3.startAnimation(rotateIn);
+
+        //next activity
+        new Handler().postDelayed(()->{
+            Intent intent = new Intent(SplashScreen.this, ChooseTarget.class);
+            startActivity(intent);
+            finish();
+        },SPLASH_SCREEN);
+    }
+}
