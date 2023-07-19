73f2b04ec0a94b641afd89cb8f5b0fd595dba295
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-19 14:24:42 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/mygymnasium/WeightUnits.java
@@ -0,0 +1,54 @@
+package com.daghlas.mygymnasium;
+
+import java.util.ArrayList;
+
+public class WeightUnits {
+
+    private static ArrayList<WeightUnits> unitsArrayList;
+    private int id;
+    private String name;
+
+    public WeightUnits(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void initUnits(){
+
+        unitsArrayList = new ArrayList<>();
+
+        WeightUnits kilograms = new WeightUnits(0, "kg");
+        unitsArrayList.add(kilograms);
+
+        WeightUnits pounds = new WeightUnits(0, "lbs");
+        unitsArrayList.add(pounds);
+    }
+
+    public static ArrayList<WeightUnits> getUnitsArrayList() {
+        return unitsArrayList;
+    }
+
+    public static String[] unitNames(){
+        String[] units = new String[unitsArrayList.size()];
+        for(int i = 0; i<unitsArrayList.size(); i++){
+            units[i] = unitsArrayList.get(i).name;
+        }
+        return units;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
