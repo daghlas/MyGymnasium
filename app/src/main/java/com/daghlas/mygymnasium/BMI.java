8c0493aa03ecadc77fce2ec1e075aa30bc0803a4
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-25 09:07:52 +0300

    initial commit

--- app/src/main/java/com/daghlas/mygymnasium/BMI.java
+++ app/src/main/java/com/daghlas/mygymnasium/BMI.java
@@ -7,6 +7,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +17,7 @@ public class BMI extends AppCompatActivity {
     ImageView back, graph;
     SharedPreferences preferences;
     TextView targetBMI,currentBMI, bmiStatement;
+    Button next;
 
     @SuppressLint("DefaultLocale")
     @Override
@@ -65,6 +68,16 @@ public class BMI extends AppCompatActivity {
 
         back = findViewById(R.id.backButton);
         back.setOnClickListener(v -> onBackPressed());
+
+        next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BMI.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     @Override
