33f169d4b2ebe6fb3c97129d3ab39b80874973f6
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-25 09:08:01 +0300

    initial commit

--- app/src/main/java/com/daghlas/mygymnasium/MainActivity.java
+++ app/src/main/java/com/daghlas/mygymnasium/MainActivity.java
@@ -1,6 +1,7 @@
 package com.daghlas.mygymnasium;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+
     }
 }
