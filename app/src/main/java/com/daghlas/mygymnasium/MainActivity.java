254ebbd94bd2cf2f978cb40e0af80c1bad871836
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-26 09:03:13 +0300

    initial commit

--- app/src/main/java/com/daghlas/mygymnasium/MainActivity.java
+++ app/src/main/java/com/daghlas/mygymnasium/MainActivity.java
@@ -3,7 +3,12 @@ package com.daghlas.mygymnasium;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.blue));
 
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setTitle(Html.fromHtml("<font color='#FFFFFFFF'>Ashley's GYM App</font>"));
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue)));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
