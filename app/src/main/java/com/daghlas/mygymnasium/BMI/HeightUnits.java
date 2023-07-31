70cc3a6006b6b9ea61fba66fee4a30904c965d5d
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-07-31 11:13:04 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/mygymnasium/BMI/HeightUnits.java
@@ -0,0 +1,55 @@
+package com.daghlas.mygymnasium.BMI;
+
+import java.util.ArrayList;
+
+public class HeightUnits {
+
+    private static ArrayList<HeightUnits> unitsArrayList;
+    private int id;
+    private String name;
+
+    public HeightUnits(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void initUnits(){
+
+        unitsArrayList = new ArrayList<>();
+
+        HeightUnits centimeters = new HeightUnits(0, "cm");
+        unitsArrayList.add(centimeters);
+
+        //HeightUnits feet = new HeightUnits(0, "ft");
+        //unitsArrayList.add(feet);
+    }
+
+    public static ArrayList<HeightUnits> getUnitsArrayList() {
+        return unitsArrayList;
+    }
+
+    public static String[] unitNames(){
+        String[] units = new String[unitsArrayList.size()];
+        for(int i = 0; i<unitsArrayList.size(); i++){
+            units[i] = unitsArrayList.get(i).name;
+        }
+        return units;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
