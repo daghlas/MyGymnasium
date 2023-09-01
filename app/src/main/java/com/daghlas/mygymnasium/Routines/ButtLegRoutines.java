7267a79f6cc3e0aa8bc30fc988c246bacdb5e81f
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-09-01 09:17:01 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/mygymnasium/Routines/ButtLegRoutines.java
@@ -0,0 +1,76 @@
+package com.daghlas.mygymnasium.Routines;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+
+import com.daghlas.mygymnasium.R;
+import com.daghlas.mygymnasium.Workouts.ButtLeg.ButtLegWorkout;
+import com.daghlas.mygymnasium.Workouts.FullBody.FullBodyWorkout;
+
+public class ButtLegRoutines extends AppCompatActivity {
+
+    Button startWorkout, startPreWorkout, startPostWorkout, startYoga;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_butt_leg_routines);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.blue));
+
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setTitle(Html.fromHtml("<font color='#FFFFFFFF'>Routines</font>"));
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue)));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        startWorkout = findViewById(R.id.startWorkout);
+        startWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ButtLegRoutines.this, ButtLegWorkout.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        startPreWorkout = findViewById(R.id.startPreWorkout);
+        startPreWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ButtLegRoutines.this, PreWorkoutRoutines.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+        startPostWorkout = findViewById(R.id.startPostWorkout);
+        startPostWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ButtLegRoutines.this, PostWorkoutRoutines.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+        startYoga = findViewById(R.id.startYoga);
+        startYoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ButtLegRoutines.this, YogaRoutines.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
