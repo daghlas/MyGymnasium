b5feea092e32e5142db2116cb07969fd7e207df6
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-09-01 09:17:21 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/mygymnasium/Workouts/ButtLeg/ButtLegWorkout.java
@@ -0,0 +1,46 @@
+package com.daghlas.mygymnasium.Workouts.ButtLeg;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Html;
+
+import com.daghlas.mygymnasium.R;
+import com.daghlas.mygymnasium.Routines.ButtLegRoutines;
+import com.daghlas.mygymnasium.Routines.FullBodyRoutines;
+import com.daghlas.mygymnasium.Workouts.FullBody.FullBodyWorkout;
+
+public class ButtLegWorkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_butt_leg_workout);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.blue));
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(Html.fromHtml("<font color='#FFFFFFFF'>Butt & Leg Workouts</font>"));
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue)));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(ButtLegWorkout.this, ButtLegRoutines.class);
+        startActivity(intent);
+        finish();
+        super.onBackPressed();
+    }
+}
