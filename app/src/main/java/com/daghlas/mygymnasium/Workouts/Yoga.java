11186eb60e0b615d52bd71368baa8943fe7f3a88
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-11-09 09:49:24 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/mygymnasium/Workouts/Yoga.java
@@ -0,0 +1,28 @@
+package com.daghlas.mygymnasium.Workouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Html;
+
+import com.daghlas.mygymnasium.R;
+
+public class Yoga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yoga);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.blue));
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(Html.fromHtml("<font color='#FFFFFFFF'>Yoga</font>"));
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue)));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+}
