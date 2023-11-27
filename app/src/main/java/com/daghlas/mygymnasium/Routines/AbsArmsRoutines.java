f32d3909e1b521e2c86a665fb62e88bed7d58033
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-11-27 22:13:29 +0300

    initial commit

--- app/src/main/java/com/daghlas/mygymnasium/Routines/AbsArmsRoutines.java
+++ app/src/main/java/com/daghlas/mygymnasium/Routines/AbsArmsRoutines.java
@@ -37,13 +37,10 @@ public class AbsArmsRoutines extends AppCompatActivity {
             finish();
         });
         startPreWorkout = findViewById(R.id.startPreWorkout);
-        startPreWorkout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AbsArmsRoutines.this, PreWorkoutRoutines.class);
-                startActivity(intent);
-                //finish();
-            }
+        startPreWorkout.setOnClickListener(v -> {
+            Intent intent = new Intent(AbsArmsRoutines.this, PreWorkoutRoutines.class);
+            startActivity(intent);
+            //finish();
         });
         startPostWorkout = findViewById(R.id.startPostWorkout);
         startPostWorkout.setOnClickListener(new View.OnClickListener() {
