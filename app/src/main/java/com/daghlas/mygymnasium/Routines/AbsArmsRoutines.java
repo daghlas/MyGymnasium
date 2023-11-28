2c3c5c1cbbde44eedee737c0b1d2d7e3fd17ffeb
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-11-28 22:07:03 +0300

    initial commit

--- app/src/main/java/com/daghlas/mygymnasium/Routines/AbsArmsRoutines.java
+++ app/src/main/java/com/daghlas/mygymnasium/Routines/AbsArmsRoutines.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.View;
 import android.widget.Button;
 
 import com.daghlas.mygymnasium.R;
