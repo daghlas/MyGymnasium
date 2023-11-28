b7f3db255e74d563be96bf47967415c614737ec3
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-11-28 22:08:28 +0300

    initial commit

--- app/src/main/java/com/daghlas/mygymnasium/Routines/ButtLegRoutines.java
+++ app/src/main/java/com/daghlas/mygymnasium/Routines/ButtLegRoutines.java
@@ -54,14 +54,11 @@ public class ButtLegRoutines extends AppCompatActivity {
             }
         });
         startYoga = findViewById(R.id.startYoga);
-        startYoga.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(ButtLegRoutines.this, "Coming soon", Toast.LENGTH_SHORT).show();
-                //Intent intent = new Intent(ButtLegRoutines.this, YogaRoutines.class);
-                //startActivity(intent);
-                //finish();
-            }
+        startYoga.setOnClickListener(v -> {
+            Toast.makeText(ButtLegRoutines.this, "Coming soon", Toast.LENGTH_SHORT).show();
+            //Intent intent = new Intent(ButtLegRoutines.this, YogaRoutines.class);
+            //startActivity(intent);
+            //finish();
         });
     }
 
