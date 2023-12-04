47aebbe77072d5cc2e78a28741e442842ad399d8
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-12-04 21:05:38 +0300

    initial commit

--- app/src/main/java/com/daghlas/mygymnasium/BMI/TargetWeight.java
+++ app/src/main/java/com/daghlas/mygymnasium/BMI/TargetWeight.java
@@ -6,7 +6,6 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
